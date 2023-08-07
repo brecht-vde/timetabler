90850cc66b497ee868383344c490919bd94bbf9d
Brecht Van der Elst <brechtvanderelst@gmail.com>
2023-08-07 22:59:36 +0200

    move pages around

--- timetable/src/routes/+layout.svelte
+++ timetable/src/routes/+layout.svelte
@@ -5,6 +5,20 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+	import { AppShell, LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<div class="page-container flex flex-row justify-between p-4">
+			<h1 class="h3">Timetabler</h1>
+			<LightSwitch />
+		</div>
+	</svelte:fragment>
+	<div class="page-container">
+		<slot />
+	</div>
+	<svelte:fragment slot="footer">
+		<div class="page-container flex flex-row justify-center p-4">© Brecht Van der Elst</div>
+	</svelte:fragment>
+</AppShell>
---
+++ timetable/src/routes/patients/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Button from '$lib/components/common/button.svelte';
+	import PatientCard from '$lib/components/patient/patient-card.svelte';
+	import { Availabilities } from '$lib/data/data';
+	import { Patient } from '$lib/logic/domain/types';
+	import { createPatient, patientsStore } from '$lib/stores/patients-store';
+
+	const onCreatePatient = () => {
+		createPatient(Patient.createPatient('Patient', Availabilities));
+	};
+</script>
+
+<div class="flex flex-row justify-between">
+	<h1>Patients</h1>
+	<Button class="btn-icon variant-filled-primary" on:click={onCreatePatient}>+</Button>
+</div>
+
+<div class="grid grid-cols-5 gap-4">
+	{#each Object.values($patientsStore) as patient (patient.id)}
+		<PatientCard {patient} />
+	{/each}
+</div>
