30328b4640a30ac63e5d2cb8637f69520a07ba3b
Brecht Van der Elst <brechtvanderelst@gmail.com>
2023-08-07 22:59:22 +0200

    add availability table

--- timetable/src/lib/components/availabilities/availability-table.svelte
+++ timetable/src/lib/components/availabilities/availability-table.svelte
@@ -1,41 +1,73 @@
+<script lang="ts" context="module">
+	export interface AvailabilityChanged extends CustomEventInit {
+		availability: Availability;
+	}
+</script>
+
 <script lang="ts">
 	import { Days, Slots } from '$lib/data/data';
-	import type { Availability } from '$lib/logic/domain/types';
-	import { find } from 'ramda';
+	import type { Availability, Slot } from '$lib/logic/domain/types';
+	import { ascend, find, groupBy, map, pipe, sort, toPairs } from 'ramda';
 	import Checkbox from '../common/checkbox.svelte';
+	import { createEventDispatcher } from 'svelte';
 
+	// Variables
 	export let availabilities: Availability[];
 
-	const findAvailability = (day: number, slot: number) =>
-		find((a: Availability) => a.day === day && a.slot === slot, availabilities);
+	// Logic
+	const groups: [string, Availability[]][] = pipe(
+		groupBy<Availability>((a) => a.slot.toString()),
+		toPairs,
+		map<[string, unknown], [string, Availability[]]>(([key, group]) => [
+			key,
+			sort(
+				ascend((a: Availability) => a.day),
+				group as Availability[]
+			)
+		])
+	)(availabilities) as [string, Availability[]][];
 
-	const onCheckedChanged = (availability: Availability | undefined) => {
-		if (availability) {
-			alert(JSON.stringify(availability));
-		}
+	const getSlotLabel = (id: string) =>
+		pipe(
+			find((s: Slot) => s.id === parseInt(id)),
+			(s: Slot | undefined) => s?.label || 'N/A'
+		)(Slots);
+
+	// Events
+	const dispatch = createEventDispatcher();
+
+	const availabilityChanged = (availability: Availability) => {
+		const event: AvailabilityChanged = {
+			availability: {
+				...availability,
+				available: !availability.available
+			}
+		};
+
+		dispatch('availabilitychanged', event);
 	};
 </script>
 
 <div class="table-container">
-	<table class="table">
+	<table class="table table-compact">
 		<thead>
 			<tr>
-				<th />
+				<th class="table-cell-fit" />
 				{#each Days as day}
-					<th>{day.label}</th>
+					<th class="table-cell-fit">{day.label}</th>
 				{/each}
 			</tr>
 		</thead>
 		<tbody>
-			{#each Slots as slot}
+			{#each groups as [key, group]}
 				<tr>
-					<th>{slot.label}</th>
-					{#each Days as day}
-						<td>
+					<th class="table-cell-fit">{getSlotLabel(key)}</th>
+					{#each group as availability}
+						<td class="table-cell-fit">
 							<Checkbox
 								class="checkbox"
-								checked={findAvailability(day.id, slot.id)?.available}
-								on:change={() => onCheckedChanged(findAvailability(day.id, slot.id))}
+								checked={availability.available}
+								on:change={() => availabilityChanged(availability)}
 							/>
 						</td>
 					{/each}
