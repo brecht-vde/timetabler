5bab121e40c985599013b14dd19968598fd91b6f
Brecht Van der Elst <brechtvanderelst@gmail.com>
2023-08-07 22:58:04 +0200

    implement local storage store

--- timetable/src/lib/stores/patients-store.ts
+++ timetable/src/lib/stores/patients-store.ts
@@ -1,8 +1,9 @@
-import type { Patient } from '$lib/logic/domain/types';
+import type { Availability, Patient } from '$lib/logic/domain/types';
 import { localStorageStore } from '@skeletonlabs/skeleton';
+import { map } from 'ramda';
 import type { Writable } from 'svelte/store';
 
-const patientsStore: Writable<{ [id: number]: Patient }> = localStorageStore('patientsStore', {});
+const patientsStore: Writable<{ [id: string]: Patient }> = localStorageStore('patientsStore', {});
 
 const createPatient = (patient: Patient) => {
 	patientsStore.update((current) => ({
@@ -11,18 +12,34 @@ const createPatient = (patient: Patient) => {
 	}));
 };
 
-const updatePatient = (id: number, updates: Partial<Patient>) => {
+const updatePatient = (id: string, updates: Partial<Patient>) => {
 	patientsStore.update((current) => ({
 		...current,
 		[id]: { ...current?.[id], ...updates }
 	}));
 };
 
-const deletePatient = (id: number) => {
+const deletePatient = (id: string) => {
 	patientsStore.update((current) => {
 		const { [id]: _, ...remainingPatients } = current;
 		return remainingPatients;
 	});
 };
 
-export { patientsStore, createPatient, updatePatient, deletePatient };
+const updateAvailability = (id: string, update: Availability) => {
+	patientsStore.update((current) => ({
+		...current,
+		[id]: {
+			...current[id],
+			availabilities: map(
+				(a: Availability) =>
+					a.day === update.day && a.slot === update.slot
+						? { ...a, available: update.available }
+						: a,
+				current[id].availabilities
+			)
+		}
+	}));
+};
+
+export { patientsStore, createPatient, updatePatient, deletePatient, updateAvailability };
