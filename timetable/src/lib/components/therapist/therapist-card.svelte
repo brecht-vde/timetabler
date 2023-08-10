a4420587ab11f6c87b3a5d862c01ab3d2890db10
Brecht Van der Elst <brechtvanderelst@gmail.com>
2023-08-11 00:37:15 +0200

    add therapist components

---
+++ timetable/src/lib/components/therapist/therapist-card.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+	import type { Availability, Therapist } from '$lib/logic/domain/types';
+	import {
+		deleteTherapist,
+		updateAvailability,
+		updateTherapist
+	} from '$lib/stores/therapists-store';
+	import AvailabilityTable from '../availabilities/availability-table.svelte';
+	import Button from '../common/button.svelte';
+	import InteractiveLabel from '../common/interactive-label.svelte';
+	import { Trash2 } from 'lucide-svelte';
+	import PatientSelector, { type PatientChanged } from '../patient/patient-selector.svelte';
+	import { filter } from 'ramda';
+
+	// Variables
+	export let therapist: Therapist;
+	let label: string = therapist.label || 'Therapist';
+
+	// Events
+	const onAvailabilityChanged = (availability: Availability) => {
+		updateAvailability(therapist.id, availability);
+	};
+
+	const onDeleteTherapist = () => {
+		deleteTherapist(therapist.id);
+	};
+
+	const onUpdateTherapist = () => {
+		therapist.label = label;
+		updateTherapist(therapist.id, therapist);
+	};
+
+	const onUpdateDedicatedPatient = (detail: PatientChanged) => {
+		therapist.dedicated = detail.action === 'add' ? detail.patient.id : undefined;
+		updateTherapist(therapist.id, therapist);
+	};
+
+	const onUpdateExcludedPatient = (detail: PatientChanged) => {
+		if (!therapist.excluded) {
+			therapist.excluded = [];
+		}
+
+		if (detail.action === 'remove') {
+			therapist.excluded = filter((id: string) => id !== detail.patient.id, therapist.excluded);
+		} else if (detail.action === 'add') {
+			therapist.excluded.push(detail.patient.id);
+		}
+
+		updateTherapist(therapist.id, therapist);
+	};
+</script>
+
+<div class="card">
+	<header class="card-header">
+		<div class="flex flex-row gap-4">
+			<InteractiveLabel placeholder="Therapist" on:click={onUpdateTherapist} bind:value={label} />
+			<div>
+				<Button class="btn btn-icon btn-icon-sm variant-ghost-error" on:click={onDeleteTherapist}>
+					<Trash2 class="icon-sm" />
+				</Button>
+			</div>
+		</div>
+	</header>
+	<section class="flex flex-col p-4 gap-4">
+		<PatientSelector
+			multi={false}
+			label="Dedicated patient"
+			selectedIds={therapist.dedicated ? [therapist.dedicated] : []}
+			on:patientchanged={(e) => onUpdateDedicatedPatient(e.detail)}
+		/>
+		<PatientSelector
+			multi={true}
+			label="Excluded patients"
+			selectedIds={therapist.excluded ? therapist.excluded : []}
+			on:patientchanged={(e) => onUpdateExcludedPatient(e.detail)}
+		/>
+		<AvailabilityTable
+			label="Availability"
+			availabilities={therapist.availabilities}
+			on:availabilitychanged={(e) => onAvailabilityChanged(e.detail.availability)}
+		/>
+	</section>
+</div>
---
+++ timetable/src/lib/components/therapist/therapists-panel.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Button from '$lib/components/common/button.svelte';
+	import TherapistCard from '$lib/components/therapist/therapist-card.svelte';
+	import { Availabilities } from '$lib/data/data';
+	import { Therapist } from '$lib/logic/domain/types';
+	import { createTherapist, therapistsStore } from '$lib/stores/therapists-store';
+
+	const onCreateTherapist = () => {
+		createTherapist(Therapist.createTherapist('Therapist', Availabilities));
+	};
+</script>
+
+<div class="panel-container-columns">
+	<div>
+		<Button class="btn btn-sm variant-filled-secondary" on:click={onCreateTherapist}>+add</Button>
+	</div>
+	<div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-4">
+		{#each Object.values($therapistsStore) as therapist (therapist.id)}
+			<TherapistCard {therapist} />
+		{/each}
+	</div>
+</div>
